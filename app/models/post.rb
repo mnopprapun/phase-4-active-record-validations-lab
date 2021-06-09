7f5308ae1be63f93879f72e4d4ecdba01c3847b8
Marc Nopprapun <mnopprapun@gmail.com>
2021-06-09 11:15:03 -0500

    commit tree 72c706ff1d75bf29b829e9dbb48e3a49b1f08daa

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+	
+	validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+	validate :clickbait?
+
+	PATTERNS = [/Won't Believe/i]
+
+	def clickbait?
+	if PATTERNS.none?{|pattern| pattern.match title}
+	errors.add(:title, "is clickbait")
+	end
+	end	
 end
